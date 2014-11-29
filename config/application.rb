001050c256ff88650a3aa95c325e839dfad9c783
kon.yuichi <konpyu@gmail.com>
2014-11-29 17:47:16 +0900

    ui test

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Cityscape
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.paths << Rails.root.join("lib", "assets", "bower_components")
     config.autoload_paths += %W(#{config.root}/lib)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
